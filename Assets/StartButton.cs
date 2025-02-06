af47c4c2484b16fd634a93f51b47111a3273065a
nr687 <nr687@scarletmail.rutgers.edu>
2025-02-05 19:44:01 -0500

    Gold apple addition

--- Assets/StartButton.cs
+++ Assets/StartButton.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class StartButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+  
     void Start()
     {
-        SceneManager.LoadScene( "_Scene_0" );
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        SceneManager.LoadScene( "_Scene_0" );
     }
 }
